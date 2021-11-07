45087dc720e8d38ba8bf40be6816fb41cd023363
MateuszKost <matekos1604@gmail.com>
2021-11-08 00:10:59 +0100

    Created DataAccessLayerFactory

---
+++ DataAccessLayer/DataAccessLayerFactory.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer
+{
+    public class DataAccessLayerFactory
+    {
+        private static IContainer Container { get; }
+        static DataAccessLayerFactory()
+        {
+            ContainerBuilder builder = new ContainerBuilder();
+            builder.RegisterType<DbContextOptions<MyTutoringContext>>();
+            builder.RegisterType<MyTutoringContext>().As<IMyTutoringContext>();
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>();
+            Container = builder.Build();
+        }
+
+        public static IUnitOfWork CreateUnitOfWork()
+        {
+            return Container.Resolve<IUnitOfWork>();
+        }
+    }
+}
